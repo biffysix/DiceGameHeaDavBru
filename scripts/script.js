7bd9910b28bcfe473767bc60be0980bba9a490a1
biffysix <mrbrucewong@gmail.com>
2020-11-22 05:25:09 -0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,42 @@ class Die{
     }
 }
 
+// Part 2
+// Die Object
+class DiceRoll{
+
+    constructor(){
+        this.values = [1,2,3,4,5,6];
+    
+    
+    }
+}
+// Part 2a
+DiceRoll.prototype.roll = function(){
+
+    let i,j,x, z;
+    for( i=this.values.length-1; i>0; i-- ){
+        //randomly select a die roll value
+        j = Math.floor(Math.random() * (i+1));
+        x=this.values[j];
+        return z= output.innerHTML += `<p> ${x}`;
+    }
+}
+
+// Part 2b
+DiceRoll.prototype.describeSelf = function(){
+    //let output = "";
+    output.innerHTML += `<p>Dice rolled a number `;
+    
+}
+
+
 //Test instantiate Die object and display value
 const myDie = new Die(6);
 
 output.innerHTML += `${myDie.describeSelf()}`;
+//Test instantiate DiceRoll object
+const myDiceRoll = new DiceRoll();
+
+myDiceRoll.describeSelf();
+myDiceRoll.roll();
